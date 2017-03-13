9b58f4a123dd26ca322bb57f66eec57663d1468d
natmark <natmark0918@gmail.com>
2017-03-13 18:32:42 +0900

    :new: add TabBarController

---
+++ inspix-iOS/src/controllers/TabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarController.swift
+//  inspix-iOS
+//
+//  Created by AtsuyaSato on 2017/03/13.
+//  Copyright © 2017年 Atsuya Sato. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
