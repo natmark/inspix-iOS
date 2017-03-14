98dd36546a4725d76c535f80beaa6fea96a39bac
natmark <natmark0918@gmail.com>
2017-03-14 15:13:19 +0900

    :pencil2: set password length

--- inspix-iOS/src/commons/Util.swift
+++ inspix-iOS/src/commons/Util.swift
@@ -8,11 +8,12 @@
 
 import Foundation
 class Util {
+    let passLength = 10
     //[a-zA-Z0-9]の乱数をStringで返す
-    func randomStringWithLength(length: Int) -> String {
+    private func randomPassGenerator() -> String {
         let base = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
         var randomString: String = ""
-        for _ in 0..<length {
+        for _ in 0..<passLength {
             let randomValue = arc4random_uniform(UInt32(base.characters.count))
             let hoge = base.index(base.startIndex, offsetBy: Int(randomValue))
             print(hoge)
