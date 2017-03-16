f996288c8d0cdcb5a6613967f131c0de49e707ff
natmark <natmark0918@gmail.com>
2017-03-17 00:01:19 +0900

    :sparkles: add timeline connection

--- inspix-iOS/src/connection/UserTimeLine.swift
+++ inspix-iOS/src/connection/UserTimeLine.swift
@@ -8,25 +8,40 @@
 
 import Foundation
 import APIKit
+import Decodable
 
-//struct GetUserTimeLineRequest : InspixRequest {
-//    let userId : String
-//    let pager : Int
-//    
-//    typealias Response = TimeLineResponse
-//    
-//    var method: HTTPMethod {
-//        return .get
-//    }
-//    var parameters: Any?{
-//        return [
-//            "user_id": userId,
-//            "page": pager
-//        ]
-//        
-//    }
-//    var path: String {
-//        return "/userTimeline"
-//    }    
-//}
-//
+struct GetUserTimeLineRequest : InspixRequest {
+    let userId : String
+    let pager : Int
+    
+    typealias Response = TimeLineResponse
+    
+    var method: HTTPMethod {
+        return .get
+    }
+    var parameters: Any?{
+        return [
+            "user_id": userId,
+            "page": pager
+        ]
+        
+    }
+    var path: String {
+        return "/userTimeline"
+    }
+    func response(from object: Any, urlResponse: HTTPURLResponse) throws -> UserResponse {
+        return try UserResponse.decode(object)
+    }
+}
+struct TimeLineResponse: Decodable {
+    let user : User
+    let inspirations : [Inspiration]
+    
+    public static func decode(_ json: Any) throws -> TimeLineResponse {
+        return try TimeLineResponse(
+            user: json => "data" => "user",
+            inspirations: json => "data" => "inspirations"
+        )
+    }
+}
+
---
+++ inspix-iOS/src/models/User.swift
@@ -0,0 +1,30 @@
+//
+//  User.swift
+//  inspix-iOS
+//
+//  Created by AtsuyaSato on 2017/03/16.
+//  Copyright © 2017年 Atsuya Sato. All rights reserved.
+//
+
+import Foundation
+import Decodable
+
+struct User:Decodable {
+    let id: Int
+    let name: String
+    let follow: Int
+    let follower: Int
+    let following: Bool
+    let thumbnailImage : String
+    
+    static func decode(_ json: Any) throws -> User {
+        return try User(
+            id: json => "id",
+            name: json => "name",
+            follow: json => "follow",
+            follower: json => "follower",
+            following: json => "following",
+            thumbnailImage: json => "thumbnail_image"
+        )
+    }
+}
